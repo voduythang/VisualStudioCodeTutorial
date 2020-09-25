5e398b93043bb2c70611ff76abb981f40b22acba
Vo Duy Thang <dvo@mantu.com>
2020-09-25 20:18:13 +0700

    consume a nugetpackage

---
+++ ConsumeANugetPackage/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ConsumeANugetPackage
+{
+    public class Account
+    {
+        public string Name {get; set;}
+        public string Email {get; set;}
+        public DateTime DOB {get; set;}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acount = new Account
+            {
+                Name = "May Lang Thang",
+                Email = "maylangthang@maylangthang.com",
+                DOB = new DateTime(1987,8,1,0,0,0, DateTimeKind.Utc)
+            };
+            string json = JsonConvert.SerializeObject(acount,Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
